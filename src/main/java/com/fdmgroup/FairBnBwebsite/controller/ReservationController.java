9de824f6ff1efac29b278f4f40180bfc7afe6757
Shiv Kumar <shiv.kumar@fdmgroup.com>
2019-10-31 12:19:41 +0000

    half finished reservation entity, edit function not working, add sort of works - Shiv

--- src/main/java/com/fdmgroup/FairBnBwebsite/controller/ReservationController.java
+++ src/main/java/com/fdmgroup/FairBnBwebsite/controller/ReservationController.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.fdmgroup.FairBnBwebsite.model.ContactDetail;
 import com.fdmgroup.FairBnBwebsite.model.Customer;
+import com.fdmgroup.FairBnBwebsite.model.PropertyType;
 import com.fdmgroup.FairBnBwebsite.model.Reservation;
 import com.fdmgroup.FairBnBwebsite.service.ReservationService;
 
@@ -46,4 +49,33 @@ public class ReservationController {
 		return "index-reservations";
 	}
 	
+	@GetMapping("/editreservation/{id}")
+	public String reservationUpdateForm(@PathVariable("id") int reservationId, Model model) {
+		Reservation reservation = reservationService.getReservationById(reservationId);
+		model.addAttribute("reservationAttr", reservation);
+		return "update-reservation";
+
+	}
+
+	@PostMapping("/updatereservation/{id}")
+	public String updateReservation(@PathVariable("id") int reservationId,
+			@Valid Reservation reservation, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "update-reservation";
+		}
+		reservation.setReservationId(reservationId);
+		reservationService.editReservation(reservation);
+		model.addAttribute("reservationAttr", reservationService.getAllReservations());
+		return "index-reservations";
+	}
+	
+	
+	@GetMapping("/deletereservation/{id}")
+	public String deleteReservation(@PathVariable("id") int reservationId, Model model) {
+		reservationService.deleteRedervationById(reservationId);
+		model.addAttribute("reservationAttr", reservationService.getAllReservations());
+		return "index-reservations";
+	}
+	
+
 }
